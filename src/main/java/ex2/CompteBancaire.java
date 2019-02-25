add976b64ec2006a7c6ef078f0e38b839efed9de
Alex <alex.thepaut33@gmail.com>
2019-02-25 14:45:59 +0100

    Exo 2

--- src/main/java/ex2/CompteBancaire.java
+++ src/main/java/ex2/CompteBancaire.java
@@ -1,47 +1,12 @@
 package ex2;
 
-/** Représente un compte bancaire de type compte courante (type=CC) ou livret A (type=LA)
+/** Repr�sente un compte bancaire de type compte courante (type=CC) ou livret A (type=LA)
  * @author DIGINAMIC
  */
-public class CompteBancaire {
+public abstract class CompteBancaire {
 
 	/** solde : solde du compte */
-	private double solde;
-	
-	/** decouvert : un découvert est autorisé seulement pour les comptes courants */
-	private double decouvert;
-	
-	/** tauxRemuneration : taux de rémunération dans le cas d'un livret A */
-	private double tauxRemuneration;
-	
-	/** Le type vaut soit CC=Compte courant, ou soit LA=Livret A */
-	private String type;
-	
-	/**
-	 * @param solde
-	 * @param decouvert
-	 * @param type
-	 */
-	public CompteBancaire(String type, double solde, double decouvert) {
-		super();
-		this.type = type;
-		this.solde = solde;
-		this.decouvert = decouvert;
-	}
-	
-	/** Ce constructeur est utilisé pour créer un compte de type Livret A
-	 * @param type = LA
-	 * @param solde représente le solde du compte
-	 * @param decouvert  représente le découvert autorisé
-	 * @param tauxRemuneration  représente le taux de rémunération du livret A
-	 */
-	public CompteBancaire(String type, double solde, double decouvert, double tauxRemuneration) {
-		super();
-		this.type = type;
-		this.solde = solde;
-		this.decouvert = decouvert;
-		this.tauxRemuneration = tauxRemuneration;
-	}
+	protected double solde;
 	
 	/** Ajoute un montant au solde
 	 * @param montant
@@ -53,24 +18,9 @@ public class CompteBancaire {
 	/** Ajoute un montant au solde
 	 * @param montant
 	 */
-	public void debiterMontant(double montant){
-		if (type.equals("CC")){
-			if (this.solde - montant > decouvert){
-				this.solde = solde - montant;
-			}	
-		}
-		else if (type.equals("LA")){
-			if (this.solde - montant > 0){
-				this.solde = solde - montant;
-			}	
-		}
-	}
+	public abstract void debiterMontant(double montant);
 	
-	public void appliquerRemuAnnuelle(){
-		if (type.equals("LA")){
-			this.solde = solde + solde*tauxRemuneration/100;
-		}
-	}
+	
 	
 	/** Getter for solde
 	 * @return the solde
@@ -84,41 +34,5 @@ public class CompteBancaire {
 	 */
 	public void setSolde(double solde) {
 		this.solde = solde;
-	}
-	/** Getter for decouvert
-	 * @return the decouvert
-	 */
-	public double getDecouvert() {
-		return decouvert;
-	}
-	/** Setter
-	 * @param decouvert the decouvert to set
-	 */
-	public void setDecouvert(double decouvert) {
-		this.decouvert = decouvert;
-	}
-	/** Getter for tauxRemuneration
-	 * @return the tauxRemuneration
-	 */
-	public double getTauxRemuneration() {
-		return tauxRemuneration;
-	}
-	/** Setter
-	 * @param tauxRemuneration the tauxRemuneration to set
-	 */
-	public void setTauxRemuneration(double tauxRemuneration) {
-		this.tauxRemuneration = tauxRemuneration;
-	}
-	/** Getter for type
-	 * @return the type
-	 */
-	public String getType() {
-		return type;
-	}
-	/** Setter
-	 * @param type the type to set
-	 */
-	public void setType(String type) {
-		this.type = type;
 	}
 }
--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,30 @@
 package ex2;
 
 public class LivretA extends CompteBancaire {
+	
+	private double tauxRemuneration;
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	@Override
+	public void debiterMontant(double montant) {
+		if (this.solde - montant > 0){
+			this.solde = solde - montant;
+		}
 	}
-
+			
+	public void appliquerRemuAnnuelle(){
+		this.solde = solde + solde*tauxRemuneration/100;
+	}
+	
+	/** Getter for tauxRemuneration
+	 * @return the tauxRemuneration
+	 */
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+	/** Setter
+	 * @param tauxRemuneration the tauxRemuneration to set
+	 */
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
+	}
 }
